b20ef6cf59e65a9f348fcc15053b1118c4a9d3f8
shamoon1997 <sshamoon83@gmail.com>
2024-03-11 03:51:53 -0400

    fix: unnecessary logs removed

--- script.js
+++ script.js
@@ -2,5 +2,3 @@ let ageSpan = document.getElementById('showAge');
 let birthYear = 1997;
 let currentYear = new Date().getFullYear();
 ageSpan.innerHTML = currentYear - birthYear;
-
-console.log("hello")
